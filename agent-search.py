0c523e7e507986dd9ee6b202c4fe04b51dddfa1f
SyingSHY <posit1x38@gmail.com>
2026-02-02 15:12:24 +0900

    feat: Tavily 기반 검색 수행 노드 구현

--- agent-search.py
+++ agent-search.py
@@ -1,8 +1,9 @@
 import os
 from typing import List, TypedDict
 from pydantic import BaseModel, Field
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
 from langgraph.graph import StateGraph, START, END
 from langsmith import traceable
 
@@ -23,9 +24,21 @@ class SearchQueries(BaseModel):
         description="검색 엔진용으로 최적화된 3~6개의 검색 쿼리 리스트"
     )
 
+class SearchItem(BaseModel):
+    title: str = Field(description="검색 결과의 제목")
+    url: str = Field(description="원본 웹페이지 링크")
+    content: str = Field(description="검색 결과의 주요 내용")
+    score: float = Field(description="쿼리와 검색 결과의 유사도 점수")
+
+class StructuredSearchOutput(BaseModel):
+    query: str
+    results: List[SearchItem]
+
 class AgentState(TypedDict):
+    is_analysis_need: bool = Field(default=True)
     question: str
     search_queries: List[str]
+    search_results: List[SearchItem]
 
 def generate_queries(state: AgentState):
     """
@@ -49,12 +62,46 @@ def generate_queries(state: AgentState):
     
     return {"search_queries": result.queries}
 
+def search_with_tavily(state:AgentState):
+    """
+    Docstring for search_with_tavily
+    
+    :param state: Description
+    :type state: AgentState
+    """
+    queries = state['search_queries']
+    search = TavilySearch(
+        max_results=3,
+        search_depth="advanced"
+    )
+    search_results = []
+
+    for query in queries:
+        output = search.invoke({"query": query})
+        for res in output["results"]:
+            item = SearchItem(
+                title=res.get("title", "No Title"),
+                url=res.get("url", ""),
+                content=res.get("content", ""),
+                score=res.get("score", 0.0)
+            )
+            search_results.append(item)
+
+    final_output = StructuredSearchOutput(
+        query=query,
+        results=search_results
+    )
+
+    return {"search_results": search_results}
+
 workflow = StateGraph(AgentState)
 
 workflow.add_node("query_gen", generate_queries)
+workflow.add_node("search", search_with_tavily)
 
 workflow.add_edge(START, "query_gen")
-workflow.add_edge("query_gen", END)
+workflow.add_edge("query_gen", "search")
+workflow.add_edge("search", END)
 
 app = workflow.compile()
 
@@ -64,6 +111,10 @@ if __name__ == '__main__':
 
     output = app.invoke(inputs, config)
 
-    print("\n--- [최종 결과] ---")
+    print("\n--- [쿼리 생성 결과] ---")
     for i, q in enumerate(output["search_queries"], 1):
         print(f"{i}. {q}")
+
+    print("\n--- [검색 결과] ---")
+    for i, r in enumerate(output["search_results"], 1):
+        print(f"{i}. {r}")
