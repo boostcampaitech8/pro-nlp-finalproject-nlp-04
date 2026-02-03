69809a7ff429cef6a9391604de7055fdb623ba30
orzalro <orzalro@gmail.com>
2026-02-03 15:57:29 +0900

    feature: .env를 통한 api key 관리 및 upstage api 호출 추가

--- src/main.py
+++ src/main.py
@@ -3,15 +3,31 @@ from graph.supervisor_graph import supervisor_app
 
 
 # 테스트
-initial_state = GlobalState()
+initial_state = {
+    "messages": [],
+    "awaiting_input": False,
+    "input_request": None,
+    "user_response": None,
+    "current_task": None,
+    "idea": {
+        "planning_style": "",
+        "rationale": "",
+        "toc": [],
+    },
+    "supervision": {
+        "stage": "init",
+        "user_intent": "",
+        "last_decision": "",
+    },
+}
+
 while True:
     state = supervisor_app.invoke(initial_state)
-
+    print(state)
+    
     if state.get("awaiting_input"):
         print(state["input_request"])
         user_input = input("> ")
 
-        state["awaiting_input"] = False
         state["user_response"] = user_input
-        state["messages"] = {"role": "user", "content": user_input}
         initial_state = state
